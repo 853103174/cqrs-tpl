4ff8bb3e54a2fad4f93859555888822759a7c6ff
Ray <zhanglei@jiajiayue.com.cn>
2023-08-28 18:23:08 +0800

    IDEA环境配置

--- common/src/main/java/com/sdnc/common/beetlsql/CustomizeXMLClasspathLoader.java
+++ common/src/main/java/com/sdnc/common/beetlsql/CustomizeXMLClasspathLoader.java
@@ -1,31 +1,31 @@
 package com.sdnc.common.beetlsql;
 
-import java.net.URL;
-
 import org.beetl.sql.core.SqlId;
 import org.beetl.sql.xml.XMLClasspathLoader;
 
+import java.net.URL;
+
 public class CustomizeXMLClasspathLoader extends XMLClasspathLoader {
 
-    public CustomizeXMLClasspathLoader(String root, String charset) {
-        super(root, charset);
+	public CustomizeXMLClasspathLoader(String root, String charset) {
+		super(root, charset);
 
-    }
+	}
 
-    public CustomizeXMLClasspathLoader(String root) {
-        super(root);
-    }
+	public CustomizeXMLClasspathLoader(String root) {
+		super(root);
+	}
 
-    public CustomizeXMLClasspathLoader() {
-        this("sql");
-    }
+	public CustomizeXMLClasspathLoader() {
+		this("sql");
+	}
 
-    @Override
-    protected URL getFilePath(String root, SqlId id) {
-        String path = this.getPathBySqlId(id);
-        String filePath = root + "/" + path + "/" + path + ".xml";
+	@Override
+	protected URL getFilePath(String root, SqlId id) {
+		String path = this.getPathBySqlId(id);
+		String filePath = root + "/" + path + "/" + path + ".xml";
 
-        return this.getFile(filePath);
-    }
+		return this.getFile(filePath);
+	}
 
 }
--- trade-hall/src/main/java/com/sdnc/trade/infrastructure/dao/area/AreaDao.java
+++ trade-hall/src/main/java/com/sdnc/trade/infrastructure/dao/area/AreaDao.java
@@ -1,25 +1,20 @@
 package com.sdnc.trade.infrastructure.dao.area;
 
-import java.util.List;
-
+import com.sdnc.trade.domain.persistantobject.area.AreaPO;
+import com.sdnc.trade.domain.valueobject.area.AreaPageVO;
 import org.beetl.sql.mapper.BaseMapper;
-import org.beetl.sql.mapper.annotation.Param;
 import org.beetl.sql.mapper.annotation.SqlResource;
 import org.springframework.stereotype.Repository;
 
-import com.sdnc.trade.domain.persistantobject.area.AreaPO;
-import com.sdnc.trade.domain.valueobject.area.AreaPageVO;
+import java.util.List;
 
 /**
- *
  * 行政区划表
- *
- *
  */
 @Repository
 @SqlResource("area")
 public interface AreaDao extends BaseMapper<AreaPO> {
 
-    List<AreaPageVO> allList(@Param("parentCode") Integer parentCode);
+	List<AreaPageVO> allList(Integer parentCode);
 
 }
